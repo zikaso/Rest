0c54141ab8b72250ea4e99158138a26096a96b69
zakaria <azizi.zakaria@gmail.com>
2019-04-01 13:02:49 +0100

    add client class

---
+++ messenger/src/main/java/org/koushik/javabrains/messenger/model/Client.java
@@ -0,0 +1,32 @@
+package org.koushik.javabrains.messenger.model;
+
+public class Client {
+	private String nom;
+	private int  age;
+	private String departement;
+	
+	
+	
+	
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public String getDepartement() {
+		return departement;
+	}
+	public void setDepartement(String departement) {
+		this.departement = departement;
+	}
+	
+	
+	
+}
